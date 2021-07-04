343781f480117f85c9bc6ee4eaf078cb65507e2d
Maksim Kalenich <maxim.kalenich@gmail.com>
2021-07-04 23:50:56 +0300

    feat: init project
    CMakeList.txt added as well as other cpp code

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # nodecey
 Simple C++ binding to Node learning curve project
+
+## Step 1
+
+Let's start by building simple CMake project.
---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+//
+// Created by maxja on 7/4/2021.
+//
+#include <iostream>
+#include "config.h"
+
+int main(int argc, char* argv[]) {
+    std::cout << argv[0] << " version " << nodecey_VERSION_MAJOR << "." << nodecey_VERSION_MINOR << "\n";
+    return 0;
+}
